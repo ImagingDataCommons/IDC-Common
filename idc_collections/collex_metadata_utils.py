b77c2c32978179d9a0cb36e2a186bd3b6a0a4272
S. Paquette <spaquett@systemsbiology.org>
2025-05-08 10:18:26 -0700

    -> quotes around values

--- idc_collections/collex_metadata_utils.py
+++ idc_collections/collex_metadata_utils.py
@@ -1469,7 +1469,7 @@ def get_table_data_with_cart_data(tabletype, sortarg, sortdir, current_filters,f
                 val = current_filters[tblitem][0]
                 reg = re.compile(r'[^A-Za-z\.0-9\-_]', re.IGNORECASE)
                 val = reg.sub("?", val[:min(len(val), 256)])
-                tblfiltstr += '(+{}:{})'.format(tokenized_fields[tblitem], val)
+                tblfiltstr += '(+{}:"{}")'.format(tokenized_fields[tblitem], val)
             else:
                 tblfiltstr += '(+'+tblitem+':(' + ' OR '.join(['"' + x +'"' for x in current_filters[tblitem]]) + '))'
             del(current_filters[tblitem])
